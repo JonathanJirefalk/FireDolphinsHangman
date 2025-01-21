5dbafbaf2fdc960e81c8c5ca83ec01c5e617f98e
JonathanJirefalk <3tomu24.jirejo@folkuniversitetet.nu>
2025-01-21 15:00:32 +0100

    Fixed Issues

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
 public class App {
 
     StartGame startGame = new StartGame();
+    Guessing guessing = new Guessing();
+    Choosingwords choosingwords = new Choosingwords();
 
     public void run(){
 
         startGame.chooseNames();
+        choosingwords.chooseWord();
+        guessing.guessLetter();
     }
 }
--- src/Choosingwords.java
+++ src/Choosingwords.java
@@ -1,26 +1,21 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Choosingwords {
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        String word1 = "";
-        String word2 = "";
 
-     
-        for (int turn = 1; turn <= 2; turn++) { 
-            System.out.print("Player 1, enter a word (Turn " + turn + "): ");
-            word1 = scanner.nextLine();
+    Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Player 2, enter a word (Turn " + turn + "): ");
-            word2 = scanner.nextLine();
-        }
+    public static String word1;
+    public static String word2;
 
-        
+    public void chooseWord(){
+
+        System.out.println("Player 1, enter a word");
+        word1 = scanner.nextLine();
+
+        System.out.println("Player 2, enter a word");
+        word2 = scanner.nextLine();
+
         System.out.println("Player 1 entered: " + word1);
         System.out.println("Player 2 entered: " + word2);
- 
-        scanner.close();
     }
 }
