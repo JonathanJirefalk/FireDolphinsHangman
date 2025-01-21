1c39c00c602e88d9fd19ff80bfd3b818f3f6bae9
JonathanJirefalk <3tomu24.jirejo@folkuniversitetet.nu>
2025-01-21 15:04:45 +0100

    mergeFix

--- src/Guessing.java
+++ src/Guessing.java
@@ -3,23 +3,41 @@ import java.util.Scanner;
 public class Guessing
 {
     Scanner scanner = new Scanner(System.in);
-    
+
     public int guessingTurn = 0;
-    public char playerOneGuess;
-    public char playerTwoGuess;
-    
+    char guess;
+
     public void guessLetter(){
+
+        System.out.println(guessingTurn);
 
         if (guessingTurn == 0){
 
             System.out.println(StartGame.playerOne + "'s turn guess a letter!");
-            playerOneGuess = scanner.next().charAt(0);
+            guess = scanner.next().charAt(0);
             guessingTurn = 1;
+            String word = Choosingwords.word2;
+            checkGuess(word);
+
         }else if (guessingTurn == 1){
 
             System.out.println(StartGame.playerTwo + "'s turn guess a letter!");
-            playerTwoGuess = scanner.next().charAt(0);
+            guess = scanner.next().charAt(0);
             guessingTurn = 0;
+            String word = Choosingwords.word1;
+            checkGuess(word);
         }
+    }
+
+    public void checkGuess(String word){
+
+        for (int i = 0; i < word.length(); i++){
+
+            if (guess == word.charAt(i)){
+
+                System.out.println("Correct Guess");
+            }
+        }
+        guessLetter();
     }
 }
