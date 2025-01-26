9278e477c94b6db76d0ff5d9ab8ed1f66d51c633
JonathanJirefalk <3tomu24.jirejo@folkuniversitetet.nu>
2025-01-26 21:10:20 +0100

    Layout Guessing Finished

--- src/Layout.java
+++ src/Layout.java
@@ -1,6 +1,6 @@
  public class Layout {
-    private char[] guessedWord1;
-    private char[] guessedWord2;
+    private static char[] guessedWord1;
+    private static char[] guessedWord2;
 
     public void initializeGuesses(String word1, String word2) {
         guessedWord1 = new char[word1.length()];
@@ -19,6 +19,22 @@
 
     public char[] getGuessedWord2() {
         return guessedWord2;
+    }
+
+    public void checkGuessedWord(char[] guessedWord, char guess, int turn, String word) {
+        for (int i = 0; i < guessedWord.length; i++) {
+            if (word.charAt(i) == guess) {
+                guessedWord[i] = guess;
+            }
+        }
+
+        if(turn == 0){
+            guessedWord1 = guessedWord;
+            displayGuessedWord(guessedWord);
+        }else{
+            guessedWord2 = guessedWord;
+            displayGuessedWord(guessedWord);
+        }
     }
 
     public void displayGuessedWord(char[] guessedWord) {
